0d83411206d81a4ef5c67cb9cc5b5df6cb75d6aa
stsantia <stsantia@STANSA>
2017-03-03 13:24:19 -0500

    Add project files.

---
+++ build.ps1
@@ -0,0 +1,8 @@
+﻿. .\Build\init.ps1
+
+Invoke-psake -buildFile $ProjectRoot\Build\psake.ps1 -taskList default -parameters $parameters
+
+Write-Host "Build exit code:" $LastExitCode
+
+# Propagating the exit code so that builds actually fail when there is a problem
+exit $LastExitCode
